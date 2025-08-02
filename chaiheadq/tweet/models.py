fe104b0b7f03fbd7383085393f33d92f0e4f6317
yash0219 <yashtyagi219@gmail.com>
2025-08-02 23:51:29 +0530

    models for tweet

--- chaiheadq/tweet/models.py
+++ chaiheadq/tweet/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=280)
+    photo = models.ImageField(upload_to='tweets/', blank=True, null=True)
